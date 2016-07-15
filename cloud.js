e34789e4322568b84259279f23c3011dea4557d3
Karl Stanton <kstanton@hugeinc.com>
2016-07-15 09:49:04 -0400

    Added a player and thunder sound

--- cloud.js
+++ cloud.js
@@ -7,10 +7,15 @@
 //   method: 'PUT'
 // };
 
+var player = require('player');
+// create player instance
+var thunder = new player('./thunder-clap.mp3');
+
 var request = require('request')
 var flashInterval = 0;
 var numFlashesPerEpisode = 10;
 var maxTimeBetweenFlashes = 5 * 1000;
+
 
 function generateFlashTime () {
   return Math.floor(Math.random() * 1000);
@@ -22,22 +27,32 @@ function generateFlashTime () {
     setTimeout(function() {
       // console.log(flashInterval, numFlashesPerEpisode);
       if (flashInterval < numFlashesPerEpisode) {
+
+        // play now and callback when playend
+        if (flashInterval === 0) {
+          thunder.play(function(err, player) {
+            console.log(err, player);
+          });
+        }
+
         makeLightning(true, brightness);
         flashInterval++;
         loop();
+
       }
       else {
+        thunder.stop();
         makeLightning(false, 0);
         setTimeout(loop, maxTimeBetweenFlashes);
         flashInterval = 0;
-        console.log('☁☁☁☁☁☁')
+        // console.log('☁☁☁☁☁☁')
       }
     }, rand);
 }());
 
 
 function makeLightning (on, brightness) {
-  console.log('⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡');
+  // console.log('⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡');
   request({ url: 'http://10.10.33.105/api/30e271fb6eab239f791c67f44da98659/lights/2/state', method: 'PUT', json: {on: on, bri: brightness}}, function (error, response, body) {
     if (response.statusCode == 201) {
       // console.log(response);
