e972de43b0eedbbff4e6ec4383c24d595a78f90d
Karl Stanton <kstanton@hugeinc.com>
2016-07-15 12:50:47 -0400

    Cleaned up code a little, added more mp3s

--- cloud.js
+++ cloud.js
@@ -1,66 +1,87 @@
-// var http = require('http');
+// Wrapper for http
+var request = require('request');
 
-// var options = {
-//   host: '10.10.33.105',
-//   port: 80,
-//   path: '/api/30e271fb6eab239f791c67f44da98659/lights/2/state',
-//   method: 'PUT'
-// };
+var APIId = '30e271fb6eab239f791c67f44da98659';
 
+// Options for the Phillips Hue http server where the API requests will be hit
+var options = {
+  host: 'http://10.10.33.105',
+  port: 80,
+  path: '/api/' + APIId + '/lights/2/state',
+  method: 'PUT'
+};
+
+// Sound, rain and thunder!
 var player = require('player');
-// create player instance
-var thunder = new player('./thunder-clap.mp3');
+var rain = new player('./thunder/background-rain.mp3');
+var thunder = new player('./thunder/thunder-clap.mp3');
 
-var request = require('request')
+// Config for flash intervals
 var flashInterval = 0;
 var numFlashesPerEpisode = 10;
 var maxTimeBetweenFlashes = 5 * 1000;
 
+// Start the rain...
+rain.play();
 
+// Generate a random number between 1 sec
 function generateFlashTime () {
   return Math.floor(Math.random() * 1000);
 }
 
+// Hit the API and make the Phillips Hue light flash
+function makeLightning (turnBulbOn, bulbBrightness) {
+  // console.log('⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡');
+  request(
+    {
+      url: options.host + ':' + options.port + options.path,
+      method: options.method,
+      json: {
+        on: turnBulbOn,
+        bri: bulbBrightness
+      }
+    },
+    function (error, response, body) {
+      if (response.statusCode == 201) {
+        // console.log(response);
+      } else {
+        // console.log('error: '+ response.statusCode);
+        // console.log(body);
+      }
+    }
+  );
+}
+
+// Action loop
 (function loop() {
-    var rand = generateFlashTime();
-    var brightness = Math.floor(Math.random() * 255);
+    var timeBetweenFlashes = generateFlashTime();
+    var bulbBrightness = Math.floor(Math.random() * 255);
     setTimeout(function() {
       // console.log(flashInterval, numFlashesPerEpisode);
       if (flashInterval < numFlashesPerEpisode) {
 
-        // play now and callback when playend
+        // Play now and callback when playend
         if (flashInterval === 0) {
           thunder.play(function(err, player) {
             console.log(err, player);
           });
+
         }
 
-        makeLightning(true, brightness);
+        makeLightning(true, bulbBrightness);
         flashInterval++;
+
+        // Loop through the flash cycle
         loop();
 
       }
       else {
+        // Stop noise and flashing
         thunder.stop();
         makeLightning(false, 0);
+        // Wait between the loop starts again
         setTimeout(loop, maxTimeBetweenFlashes);
         flashInterval = 0;
-        // console.log('☁☁☁☁☁☁')
       }
-    }, rand);
+    }, timeBetweenFlashes);
 }());
-
-
-function makeLightning (on, brightness) {
-  // console.log('⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡');
-  request({ url: 'http://10.10.33.105/api/30e271fb6eab239f791c67f44da98659/lights/2/state', method: 'PUT', json: {on: on, bri: brightness}}, function (error, response, body) {
-    if (response.statusCode == 201) {
-      // console.log(response);
-    } else {
-      // console.log('error: '+ response.statusCode);
-      // console.log(body);
-    }
-  });
-
-}
-
